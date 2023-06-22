5fe4ef509629a619136fd48e59c3f5b635a4dc33
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-06-22 18:47:27 -0500

    compiled typescript

--- client/js/app.js
+++ client/js/app.js
@@ -16,6 +16,10 @@ System.register("app", [], function (exports_1, context_1) {
                 return App;
             }());
             exports_1("App", App);
+            // q: what does the cacheDom() method do?
+            // a: it caches the DOM elements that are used in the app
+            // q: what does the initEvents() method do?
+            // a: it initializes the events that are used in the app
         }
     };
 });
