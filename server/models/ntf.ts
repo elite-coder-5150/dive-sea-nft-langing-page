58dfe1eecbe100855f7c0b41457734ec0eadb2de
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-07-06 18:11:58 -0500

    defined the nft interface

---
+++ server/models/ntf.ts
@@ -0,0 +1,10 @@
+export interface Nft {
+    id: number;
+    name: string;
+    description: string;
+    image: string;
+    price: number;
+    owner: number;
+    created_at: Date;
+    updated_at: Date;
+}
