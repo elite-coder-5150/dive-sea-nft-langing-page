31b0be1ff62e7eef85f3d3b57fe087be081bfa21
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-07-06 18:01:23 -0500

    defined the query method to execute the queries

--- server/database/database.ts
+++ server/database/database.ts
@@ -1,16 +1,19 @@
 // @ts-ignore
 import mysql from 'mysql';
-class Database {
-    private static instance: Database;
-    private connection: mysql.Connection;
+import {Sequelize} from 'sequelize';
 
-    private constructor() {
-        this.connection = mysql.createConnection({
+export class Database {
+    private static instance: Database;
+    public connection: mysql.Connection;
+
+    private connect() {
+        const db = new Sequelize({
+            dialect: 'mysql',
             host: 'localhost',
-            user: 'root',
-            password: 'root',
-            database: 'dive_sea'
-        });
+            port: 8889,
+            database:'dive_sea',
+            username: 'root'
+        })
     }
 
     public static getInstance(): Database {
@@ -25,15 +28,26 @@ class Database {
         return this.connection;
     }
 
-    public query (sql: string, args?: any): Promise<any> {
-        return new Promise((resolve, reject) => {
-            this.connection.query(sql, args, (err, rows) => {
-                if (err) {
-                    return reject(err);
-                }
-
-                resolve(rows);
-            });
-        });
+    public query (sql: string, args?: any) {
+        return
     }
+
+
+    // q: how can i use the function above?
+    // public query (sql: string, args?: any): Promise<any> {
+    //     return new Promise((resolve, reject) => {
+    //         this.connection.query(sql, args, (err, rows) => {
+    //             if (err) {
+    //                 return reject(err);
+    //             }
+    //
+    //             resolve(rows);
+    //         });
+    //     });
+    // }
+    // q: how can i call this function to query the database?
+    // a: you can call this function by using the following syntax:
+    // db.query(sql, args)
+    // q: how can i use this function to query the database?
+
 }
